f444b726df27c51846cbc0499d19401cfb1a652b
Tucker Brady <tuck.al.brady@gmail.com>
2022-05-12 00:29:45 -0600

    initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react'
+
+import Link from 'next/link';
+
+const categories = [{name: 'react', slug: 'react'}, {name: 'Web dev', slug: 'web-dev'}]
+
+const Header = () => {
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+        <div className='border-b w-full inline-block border-black-400 py-8'>
+            <div className='md:fload-left block'>
+                <Link href="/">
+                    <span className='cursor-pointer font-bold text-4xl text-black'>
+                        MTN Outlaws
+                    </span>
+                </Link>
+            </div>
+            <div className='hidden md:float-left md:contents'>
+                {categories.map((category) => (
+                    <Link key={category.slug} href={`/category/${category.slug}`}>
+                        <span className='md:float-right mt-2 align-middle text-black ml-4 font-semibold cursor-pointer'>
+                            {category.name}
+                        </span>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    </div>
+  ); 
+}
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,36 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {  
+          DEFAULT: '#556151',  
+          '50': '#FFFFFF',  
+          '100': '#FCFCFC',  
+          '200': '#E7EAE6',  
+          '300': '#D1D7CF',  
+          '400': '#BCC5B9',  
+          '500': '#A7B2A3',  
+          '600': '#91A08D',  
+          '700': '#7C8D77',  
+          '800': '#687764',  
+          '900': '#556151'
+        },
+          secondary: {  
+            DEFAULT: '#919494',  
+            '50': '#FFFFFF',  
+            '100': '#FFFFFF',  
+            '200': '#FFFFFF',  
+            '300': '#FFFFFF',  
+            '400': '#F8F9F9',  
+            '500': '#E4E4E4',  
+            '600': '#CFD0D0',  
+            '700': '#BABCBC',  
+            '800': '#A6A8A8',  
+            '900': '#919494'
+          },
+      }
+    },
   },
   plugins: [],
 }
